e068670de5f0bb8b706ce6d7622f24e261b1fdaf
Priyanshu Saraswat <saraswatpriyanshu29@gmail.com>
2024-12-04 13:59:15 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -7,6 +7,34 @@ dotenv.config({
 })
 
 connectDb()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at PORT : ${process.env.PORT}`);
+    
+  })
+})
+.catch((err) => {
+  console.log("MongoDb Connection Failed !! ", err);
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
